206a83aa91d6c4a99e6fbde5844299089ced846e
Tom Shawley <thomas.shawley1@gmail.com>
2021-05-14 15:23:40 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/TomShawley-Training/s3-bucket/aws"
-  version = "1.5.0"
+  version = "1.15.0"
   bucket_prefix = "tomshawley"
-  region = "eu-west-2"
   # insert required variables here
 }
